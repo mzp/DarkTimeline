fac41009af258d5abe222d4b9da5365669b9b840
mzp <mzpppp@gmail.com>
2011-04-16 16:29:55 +0900

    setup mongodb config for heroku

--- config/initializers/mongo_mapper.rb
+++ config/initializers/mongo_mapper.rb
@@ -2,7 +2,12 @@ database_config_file = File.join(Rails.root, "/config/database.mongo.yml")
 yaml_content = File.read(database_config_file)
 db_config = YAML::load(yaml_content)
 
-if db_config[Rails.env] && db_config[Rails.env]['adapter'] == 'mongodb'
+if ENV['MONGOHQ_URL'] then
+  require 'uri'
+  uri = URI.parse(ENV['MONGOHQ_URL'])
+  MongoMapper.connection = Mongo::Connection.new(uri.host, uri.port)
+  MongoMapper.database = uri.path.gsub(/^\//, '')
+elsif db_config[Rails.env] && db_config[Rails.env]['adapter'] == 'mongodb'
   mongo = db_config[Rails.env]
   MongoMapper.connection = Mongo::Connection.new(mongo['host'])
   MongoMapper.database = mongo['database']
