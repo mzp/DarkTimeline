acc5b1e361b848f0c79647ad249a97a84c2907e8
mzp <mzpppp@gmail.com>
2011-04-16 15:51:55 +0900

    first commit

---
+++ Gemfile
@@ -0,0 +1,13 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.0.3'
+
+group :development, :test do
+  gem "rspec-rails", ">= 2.3.0"
+end
+
+gem 'bson_ext'
+gem 'mongo_mapper', '>=0.8.2'
+gem 'rails3-generators'
+gem 'haml-rails'
+gem 'jquery-rails'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DarkTL::Application.config.secret_token = '981e8561d2e17bb0876299ab5cc62c64a9e00f4159bc2f0e3e71a4fbedf7eb540ca42399a866d5bce024c78115b6349eb69c05380dbf745f02fd63d774cb3f97'
